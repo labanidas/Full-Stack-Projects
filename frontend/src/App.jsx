c5b36ce956a19768f4e77a2a00f5d4c3f671dfef
LABANI <labanidas2001@gmail.com>
2025-01-23 19:29:06 +0530

    frontend pages + store initialized + axios instance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {Routes, Route} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import SettingsPage from './pages/SettingsPage';
+
+import { useAuthStore } from './store/useAuthStore';
+
 
 function App() {
 
+  const { authUser, checkAuth } = useAuthStore();
+
+  useEffect(()=>{
+    checkAuth()
+  }, [checkAuth])
+  
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </>
+      <div>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+
+      </div>
   )
 }
 
