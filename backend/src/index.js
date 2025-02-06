91eb4d4860975c28f0e6ec0a37b334599061f34c
LABANI <labanidas2001@gmail.com>
2025-02-06 15:55:58 +0530

    cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"; // to prsse cookie
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from './routes/auth.route.js';
@@ -12,6 +13,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser()); // allow to parse a cookie
+app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true
+}))
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
