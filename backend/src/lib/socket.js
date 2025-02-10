870a481053432909bbd8ef85e069cad34d062473
labanidas <labanidas2001@gmail.com>
2025-02-10 12:32:09 +0530

    online users

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -12,11 +12,22 @@ const io = new Server(server, {
     }
 })
 
+const userSocketMap = {};
+
 io.on("connection", (socket) => {
     console.log("user connected", socket.id);
     
+    const userId = socket.handshake.query.userId;
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // used to broadcast
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
     socket.on("disconnect", ()=>{
         console.log("user disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
     })
 })
 
